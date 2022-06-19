04d4b1192d275fe0433b479da5d47ae860fa0bbf
Jens Utbult <jens.utbult@yubico.com>
2022-06-19 22:37:38 +0200

    Initial Commit

---
+++ TKSmartCardTest/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TKSmartCardTest
+//
+//  Created by Jens Utbult on 2022-06-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TKSmartCardTest/TKSmartCardTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TKSmartCardTestApp.swift
+//  TKSmartCardTest
+//
+//  Created by Jens Utbult on 2022-06-19.
+//
+
+import SwiftUI
+
+@main
+struct TKSmartCardTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
